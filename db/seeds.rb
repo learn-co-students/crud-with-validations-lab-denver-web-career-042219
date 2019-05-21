3323b2ddfdc1be2be492fc7a1caa0bb106d2241a
JosephPR <reilly4181@gmail.com>
2019-05-20 21:49:21 -0600

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Song.destroy_all
+
+Song.create(title: "Come as you are",released: true, release_year: 1992, artist_name: "Nirvana", genre: "Grunge")
+Song.create(title: "Purple Haze",released: true, release_year: 1971, artist_name: "Hendrix", genre: "Blues")
+Song.create(title: "Soul Shine",released: true, release_year: 1977, artist_name: "The Allman Brothers", genre: "Rock")
